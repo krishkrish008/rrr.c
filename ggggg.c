bd678fdd9db30cb242c58c26744ffb877042a5d5
krishkrish008 <35235157+krishkrish008@users.noreply.github.com>
2018-01-22 16:24:20 +0530

    gggg.c

---
+++ ggggg.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() 
+{
+int a[100],n,k,i,sum=0;
+printf("enter the 2 number");
+scanf("%d%d",&n,&k);
+printf("enter the array");
+for(i=0;i<n;i++)
+{
+	
+	scanf("%d",&a[i]);
+}
+for(i=0;i<k;i++);
+
+	sum = sum + a[i];
+
+printf(" sum of kth value %d",sum);
+	
+}
